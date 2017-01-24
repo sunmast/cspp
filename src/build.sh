638b1a1248b1d41ad87c8e56c9dc48b0971afbe5
Wei Xia <sunmast@gmail.com>
2017-01-23 22:47:33 -0800

    Fix the build issue on Linux Mint 18.1

--- src/build.sh
+++ src/build.sh
@@ -5,13 +5,29 @@
 
 # Special notes for Windows: run bash scripts with Git Bash
 
+uname=$(uname)
+
+if [[ $uname =~ ^Linux* ]]
+then
+  runtime="ubuntu.16.04-x64"
+elif [[ $uname =~ ^MINGW* ]]
+then
+  runtime="win10-x64"
+elif [[ $uname =~ ^Darwin* ]]
+then
+  runtime="osx.10.12-x64"
+else
+  echo "Unable to determine the current platform"
+  exit 1
+fi
+
 if [ ! -e ../out ]
 then
   mkdir ../out
 fi
 
 dotnet restore **/project.json
-dotnet build **/project.json
+dotnet build **/project.json -r $runtime
 
 for proj in compiler corelib tester tests
 do
